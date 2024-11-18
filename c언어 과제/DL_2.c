e89a59a18bad889a35139be1d909dabd603560c1
sulin614 <jeansulin5374@naver.com>
2024-11-18 16:12:32 +0900

    Add files via upload

---
+++ c언어 과제/DL_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point {
+    int x;
+    int y;
+} POINT;
+
+int inputMenu(void) {
+    int input;
+
+    printf("메뉴입력(1.구조체 만들기, 2. 구조체 출력하기, -1. 종료) : ");
+    scanf("%d", &input);
+    return input;
+}
+
+int main() {
+    int menu;
+
+    do {
+        menu = inputMenu();
+        if(menu == 1) {
+            // 구조체 만들기
+        } else if(menu == 2) {
+            // 구조체 출력하기
+        } else if(menu == -1) {
+            printf("프로그램을 종료합니다.");
+        }
+    } while(menu > 0);
+
+    return 0;
+}
