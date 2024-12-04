4be5c2bce4ae027bf95df330c46b3f83938e1c35
sulin614 <jeansulin5374@naver.com>
2024-12-05 01:48:17 +0900

    Add files via upload

--- c언어 과제/9주차 2.c
+++ c언어 과제/9주차 2.c
@@ -10,20 +10,20 @@ int getline(char* line) {
 
 int main() {
     char input[100];
-    FILE* fp;
+    FILE* fp;              // 열기
 
-    if ((fp = fopen("output.txt", "w")) == NULL) {
+    if ((fp = fopen("output.txt", "w")) == NULL) {     //"w"--> 쓰기모드    , 파일이 존재하지 않을시 NULL 반환
         printf("error...");
         return 0;
     }
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) {     // 5번반복
         getline(input);
-        fputs(input, fp);
-        fputs("\n", fp);
+        fputs(input, fp);           // 한줄찍기
+        fputs("\n", fp);            // fputs 함수는 \n을 자동으로 넣어주지 않아서 넣어줘야 함
     }
 
-    fclose(fp);
+    fclose(fp);           // 닫기
 
     return 0;
 }
