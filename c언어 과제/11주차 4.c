f9273e2473b99bbb9a8194137694e24a4da5c7c4
sulin614 <jeansulin5374@naver.com>
2024-12-05 04:08:45 +0900

    Update 11주차 4.c

--- c언어 과제/11주차 4.c
+++ c언어 과제/11주차 4.c
@@ -1,61 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct point {
+typedef struct point{
     int x;
     int y;
 } POINT;
 
-int inputMenu(void) {
+int inputMenu(void){
     int input;
-    printf("1. ¸¸µé±â, 2. Ãâ·ÂÇÏ±â, 3. Å¬¸®¾î, 4. ÆÄÀÏÀúÀå, 5. ÆÄÀÏ·Îµå, -1. Á¾·á : \n");
+    printf("1. ë§Œë“¤ê¸°, 2. ì¶œë ¥í•˜ê¸°, 3. í´ë¦¬ì–´, 4. íŒŒì¼ì €ìž¥, 5. íŒŒì¼ë¡œë“œ, -1. ì¢…ë£Œ : \n");
     scanf("%d", &input);
     return input;
 }
 
-POINT* makePoint(void) {
+POINT* makePoint(void){
     POINT* p = (POINT*)malloc(sizeof(POINT));
     p->x = rand() % 100;
     p->y = rand() % 100;
     return p;
 }
 
-void printPoint(POINT* p[], int size) {
-    for (int i = 0; i < size; i++) {
+void printPoint(POINT* p[], int size){
+    for(int i = 0; i < size; i++){
         printf("x:%d, y:%d\n", p[i]->x, p[i]->y);
     }
 }
 
-void freePoints(POINT* p[], int size) {
-    for (int i = 0; i < size; i++) {
+void freePoints(POINT* p[], int size){
+    for(int i = 0; i < size; i++){
         free(p[i]);
     }
 }
 
-int saveFile(POINT* p[], int count) {
+int saveFile(POINT* p[], int count){
     FILE* fp;
     fp = fopen("points.bin", "ab");
-    if (fp == NULL) return 0;
-    for (int i = 0; i < count; i++) {
+    if(fp == NULL) return 0;
+    for(int i = 0; i < count; i++){
         fwrite(p[i], sizeof(POINT), 1, fp);
     }
     fclose(fp);
     return 1;
 }
 
-int loadFile(POINT* p[], int max) {
+int loadFile(POINT* p[], int max){
     FILE* fp;
     int fSize, fCount;
     fp = fopen("points.bin", "rb");
-    if (fp == NULL) return 0;
+    if(fp == NULL) return 0;
 
     fseek(fp, 0, SEEK_END);
     fSize = ftell(fp);
     fCount = fSize / sizeof(POINT);
     rewind(fp);
 
-    for (int i = 0; i < fCount; i++) {
-        if (i == max) break;
+    for(int i = 0; i < fCount; i++){
+        if(i == max) break;
         p[i] = (POINT*)malloc(sizeof(POINT));
         fread(p[i], sizeof(POINT), 1, fp);
     }
@@ -63,37 +63,37 @@ int loadFile(POINT* p[], int max) {
     return fCount;
 }
 
-int main() {
+int main(){
     int menu;
     int count = 0;
     POINT* points[100];
 
-    do {
+    do{
         menu = inputMenu();
-        switch (menu) {
-        case 1:
-            points[count] = makePoint();
-            count++;
-            break;
-        case 2:
-            printPoint(points, count);
-            break;
-        case 3:
-            system("clear");
-            break;
-        case 4:
-            if (!saveFile(points, count)) printf("file open error...\n");
-            else printf("file writing success..\n");
-            break;
-        case 5:
-            count = loadFile(points, 100);
-            break;
-        case -1:
-            freePoints(points, count);
-            printf("ÇÁ·Î±×·¥À» Á¾·áÇÕ´Ï´Ù.\n");
-            break;
+        switch(menu){
+            case 1:
+                points[count] = makePoint();
+                count++;
+                break;
+            case 2:
+                printPoint(points, count);
+                break;
+            case 3:
+                system("clear");
+                break;
+            case 4:
+                if(!saveFile(points, count)) printf("file open error...\n");
+                else printf("file writing success..\n");
+                break;
+            case 5:
+                count = loadFile(points, 100);
+                break;
+            case -1:
+                freePoints(points, count);
+                printf("í”„ë¡œê·¸ëž¨ì„ ì¢…ë£Œí•©ë‹ˆë‹¤.\n");
+                break;
         }
-    } while (menu > 0);
+    }while(menu > 0);
 
     return 0;
 }
